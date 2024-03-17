5fb3dc04126bf16fb2b1d409985ff1783ca19da2
DavidTkeshelashvili <153378776+DavidTkeshelashvili@users.noreply.github.com>
2024-03-17 22:19:37 +0400

    Add files via upload

---
+++ JSExample.js
@@ -0,0 +1,77 @@
+// const car = {
+//     type:"Fiat", 
+//     model:"500", 
+//     color:"white"
+// };
+
+// console.log("The car type is " + car.type);
+
+// -------------------------------------------------------------------------
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+// };
+
+// console.log(person.firstName + " is " + person.age + " years old.");
+
+// -------------------------------------------------------------------------
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     id: 5566,
+//     fullName: function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+// };
+
+// -------------------------------------------------------------------------
+
+// const time = new Date().getHours();
+// let greeting;
+// if (time < 10) {
+//   greeting = "Good morning";
+// } else if (time < 20) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+// console.log(greeting);
+
+// -------------------------------------------------------------------------
+
+// let text = "";
+
+// for (let i = 0; i < 5; i++) {
+//   text += "The number is " + i + "<br>";
+// }
+
+// console.log(text);
+
+// -------------------------------------------------------------------------
+
+// let text = "";
+// let i = 0;
+// while (i < 10) {
+//   text += "<br>The number is " + i;
+//   i++;
+// }
+// console.log(text);
+
+// -------------------------------------------------------------------------
+
+// function Person(first, last, age, eye) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eye;
+//   }
+  
+//   const myFather = new Person("John", "Doe", 50, "blue");
+  
+//   console.log("My father is " + myFather.age + ".");
+
+// -------------------------------------------------------------------------
